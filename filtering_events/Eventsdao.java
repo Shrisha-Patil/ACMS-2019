45f5c4142b13c2e14a6faffd558a76388cc06a66
manikya-aravind <44405100+manikya-aravind@users.noreply.github.com>
2019-03-11 07:10:50 +0530

    Update Eventsdao.java

--- filtering_events/Eventsdao.java
+++ filtering_events/Eventsdao.java
@@ -13,6 +13,36 @@ public class Eventsdao {
 	public void setTemplate(JdbcTemplate template) {    
 	    this.template = template;    
 	}
+	public List<Events> getEventByCategory(String category){    
+		String sql="select event_id,name,genre,date,poster from event where category=?";    
+	    return template.query(sql, new Object[]{category},new RowMapper<Events>(){
+	    	public Events mapRow(ResultSet rs, int row) throws SQLException {    
+	            Events ev=new Events();    
+	            ev.setEvent_id(rs.getInt("event_id"));
+				ev.setName(rs.getString("name"));
+				ev.setGenre(rs.getString("genre"));
+				ev.setDate(rs.getDate("date"));
+				ev.setPoster(rs.getString("poster"));
+
+				return ev;  
+	        }    
+	    });    
+	} 
+	public List<Events> getEventByGenre(String genre){    
+		String sql="select event_id,name,genre,date,poster from event where genre=?";    
+	    return template.query(sql, new Object[]{genre},new RowMapper<Events>(){
+	    	public Events mapRow(ResultSet rs, int row) throws SQLException {    
+	            Events eve=new Events();    
+	            eve.setEvent_id(rs.getInt("event_id"));
+				eve.setName(rs.getString("name"));
+				eve.setGenre(rs.getString("genre"));
+				eve.setDate(rs.getDate("date"));
+				eve.setPoster(rs.getString("poster"));
+
+				return eve;  
+	        }    
+	    });    
+	} 
 	public List<Events> getEvents(){    
 	    return template.query("SELECT event_id,name,genre,date,poster from event where date in (select curdate());",new RowMapper<Events>(){    
 	        public Events mapRow(ResultSet rs, int row) throws SQLException {    
