5980fb419c08e2be3107fd1a9ea223e576f0c69b
manikya-aravind <44405100+manikya-aravind@users.noreply.github.com>
2019-03-13 20:18:15 +0530

    Update Events.java

--- filtering_events/Events.java
+++ filtering_events/Events.java
@@ -1,43 +1,16 @@
 package com.amazon.model;
 
 import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class Events {
 	private int event_id;
 	private String name;
 	private String genre;
 	private Date date;
 	private String poster;
-	public int getEvent_id() {
-		return event_id;
-	}
-	public void setEvent_id(int event_id) {
-		this.event_id = event_id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getGenre() {
-		return genre;
-	}
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	public String getPoster() {
-		return poster;
-	}
-	public void setPoster(String poster) {
-		this.poster = poster;
-	}
-	
-	
+	private String category;
+	private String city;
 }
