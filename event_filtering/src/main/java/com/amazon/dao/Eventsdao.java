8b37450cf0e26221e53ccc55d899bb4fe93bb6b8
manikya-aravind <44405100+manikya-aravind@users.noreply.github.com>
2019-04-30 04:44:00 +0530

    Update Eventsdao.java

--- event_filtering/src/main/java/com/amazon/dao/Eventsdao.java
+++ event_filtering/src/main/java/com/amazon/dao/Eventsdao.java
@@ -1,8 +1,7 @@
 package com.amazon.dao;
 import java.sql.ResultSet;    
 import java.sql.SQLException;    
-import java.util.List;    
-import org.springframework.jdbc.core.BeanPropertyRowMapper;    
+import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;    
 import org.springframework.jdbc.core.RowMapper;    
 import com.amazon.model.Events;
