4f3932419d60e79822457bb6e06e242df2a3c4d5
manikya-aravind <44405100+manikya-aravind@users.noreply.github.com>
2019-04-30 04:40:56 +0530

    Update Showdao.java

--- book_events/src/main/java/com/amazon/dao/Showdao.java
+++ book_events/src/main/java/com/amazon/dao/Showdao.java
@@ -2,15 +2,11 @@ package com.amazon.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
-
-import com.amazon.model.Event;
 import com.amazon.model.Show;
 
 public class Showdao {
